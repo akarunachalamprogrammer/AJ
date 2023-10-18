c85ab2386682945391e1b7a48f1b53d4d379c8a0
ANANTHA RAMU K <126079880+anantharamukp@users.noreply.github.com>
2023-10-18 19:58:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/akarunachalamprogrammer/DQ-the-file-donor-bot DQ-the-file-donor-bot
+  git clone https://github.com/anantharamukp/AJ/DQ-the-file-donor-bot DQ-the-file-donor-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone "$UPSTREAM_REPO" DQ-the-file-donor-bot
