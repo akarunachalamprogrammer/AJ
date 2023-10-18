cdb2aae43ecd2e85de5eaf9bce40fe45d98e5502
ANANTHA RAMU K <126079880+anantharamukp@users.noreply.github.com>
2023-10-18 20:14:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,7 +70,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Please Use This Bot 👉 https://t.me/The_Publishers_bot \n or \n Search This Group 👉 https://t.me/+Z1yPQ4qCxoZjNGY1</b>")
+    await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴀɴᴅ sᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ  📌 http://t.me/+xqfxWxoePGs4ZWJl 📌             ᴀᴅᴍɪɴ 😎 -  @Anand_2K</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
@@ -726,7 +726,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/M_E_publisher")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Anand_2K")
                                 ]
                                 ]
                             )
@@ -799,7 +799,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                   InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                   InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                ],[
-                  InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/M_E_publisher")
+                  InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Anand_2K")
                  ]
                 ]
             )
@@ -1416,7 +1416,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/M_E_publisher")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/Anand_2K")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
@@ -1683,7 +1683,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs Wʜᴀᴛ I Fᴏᴜɴᴅ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}.\n\nHow To Download 👇 \nhttps://t.me/The_Publishers/203\n𝟏 - 𝐄𝐧𝐭𝐞𝐫 𝐓𝐡𝐞 𝐌𝐨𝐯𝐢𝐞 𝐍𝐚𝐦𝐞 𝐂𝐨𝐫𝐫𝐞𝐜𝐭.\n𝟐 - 𝐂𝐥𝐢𝐜𝐤 𝐭𝐡𝐞 𝐋𝐢𝐧𝐤 𝐀𝐧𝐝 𝐆𝐨 𝐭𝐨 𝐁𝐫𝐨𝐰𝐬𝐞𝐫\n𝟑 - 𝐂𝐥𝐢𝐜𝐤 𝗜 𝗔𝗺 𝗡𝗼𝘁 𝗮 𝗥𝗼𝗯𝗼𝘁 \n𝟰 - 𝐖𝐚𝐢𝐭𝐢𝐧𝐠 𝐟𝐨𝐫 𝟏𝟎 𝐭𝐨 𝟏𝟓 𝐒𝐞𝐜𝐨𝐧𝐝𝐬 𝐭𝐡𝐞𝐧 𝐒𝐜𝐫𝐨𝐥𝐥 𝐃𝐨𝐰𝐧 \n𝟓 - 𝐂𝐥𝐢𝐜𝐤 𝗢𝗽𝗲𝗻 𝗖𝗼𝗻𝘁𝗶𝗻𝘂𝗲 2 𝐓𝐢𝐦𝐞𝐬 \n𝟔 - 𝐒𝐜𝐫𝐨𝐥𝐥 𝐃𝐨𝐰𝐧 𝐖𝐚𝐢𝐭 𝐅𝐨𝐫 𝟓 𝐬𝐞𝐜𝐨𝐧𝐝 𝐚𝐧𝐝 𝐂𝐥𝐢𝐜𝐤 𝗚𝗲𝘁 𝗟𝗶𝗻𝗸"
+        cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs Wʜᴀᴛ I Fᴏᴜɴᴅ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}.</b>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
